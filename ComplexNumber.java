27cd671d0b06abe6c0aa21cfce9ee088a0d1d322
neviljacob <100426763+neviljacob@users.noreply.github.com>
2022-04-04 14:11:27 +0530

    Add files via upload

---
+++ ComplexNumber.java
@@ -0,0 +1,19 @@
+import java.util.*
+class ComplexNumber 
+{
+ 
+    int real, image;
+  
+    public ComplexNumber(int r, int i)
+    {
+        this.real = r;
+        this.image = i;
+    }
+ 
+    public void showC()
+    {
+        System.out.println(this.real + " +i " + this.image);
+    }
+
+    public complex add(ComplexNumber, ComplexNumber);
+}
